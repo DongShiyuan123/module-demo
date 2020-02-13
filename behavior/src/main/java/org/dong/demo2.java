30018b807d24f7c7c86db178ea544e2cd11115b0
Dong <807606445@qq.com>
2020-02-13 08:45:48 +0800

    第一次提交

---
+++ behavior/src/main/java/org/dong/demo2.java
@@ -0,0 +1,12 @@
+package org.dong;
+
+/**
+ * @author Dong
+ * @time 2020-02-13 5:57
+ */
+public class demo2 {
+    public static void main(String[] args) {
+        Demo1 demo1 = new Demo1();
+        demo1.Test();
+    }
+}
---
+++ common/src/main/java/org/dong/Demo1.java
@@ -0,0 +1,14 @@
+package org.dong;
+
+/**
+ * @author Dong
+ * @time 2020-02-13 5:54
+ */
+public class Demo1 {
+
+    public void Test(){
+        System.out.println("第一个测试方法");
+
+    }
+
+}
